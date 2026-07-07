0adfdf16592f81af6661089cf7c7dd43d6b9709a
Meenakshi Singh <singh99meenakshi@gmail.com>
2026-07-07 22:29:12 +0530

    Initial commit

---
+++ src/pages/page2.tsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+function Page1() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="container">
+            <button
+                className="btn"
+                onClick={() => navigate("/page2")}
+            >
+              Page2
+            </button>
+        </div>
+    );
+}
+
+export default Page1;
